bd744a7d51c6db3b9422ffbfc01e87d30e37d95b
“[Sumit <sagnihotri9710@gmail.com>
2025-01-30 19:31:46 +0530

    Python

--- VARIABLE.py
+++ VARIABLE.py
@@ -1,4 +1,10 @@
 # Variable :- A variable is a name that holds up a certain value(integers, floats, strings, boolean, etc) in the memory.
+
+# Strings
 first_name="Sumit"
 print(first_name)
 print(f"Hello {first_name}")
+
+# Integers
+age=21
+print(f"I am {age} years old")
