e8efdf5076bc4c836f469d6350e7a34026aadb6f
“[Sumit <sagnihotri9710@gmail.com>
2025-01-31 10:33:00 +0530

    Python

--- TYPECASTING.py
+++ TYPECASTING.py
@@ -1,2 +1,10 @@
 # Typecasting is the process of converting one data type to another.
 # str(), int(), float(), bool()
+str="Sumit"
+int=10
+float=10.5
+bool=True
+print(type(str))
+print(type(int))
+print(type(float))
+print(type(bool))
